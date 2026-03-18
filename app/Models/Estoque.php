2e8d87aa1cc419e7fde9c39416009143ff90bc2b
Victor Amorim <dev.victoramorim@gmail.com>
2026-03-18 20:54:18 -0300

    :tada: Project start

---
+++ app/Models/Estoque.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Estoque extends Model
+{
+    protected $table = 'tbl_produtos_em_estoque';
+    protected $primaryKey = 'produto_id';
+    protected $fillable = ['nome_produto', 'tipo_produto', 'qunatidade_produto'];
+    protected $hidden = ["created_at", "updated_at"];
+}
