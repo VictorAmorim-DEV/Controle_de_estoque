be808dd33ffefde5aa9a096f70840003523f63d1
Victor Amorim <dev.victoramorim@gmail.com>
2026-04-09 21:32:54 -0300

    feat: add API routes for product management

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\SuppliersController;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,3 +21,9 @@ Route::post('addSuppliers', [SuppliersController::class, 'addSuppliers'])->name(
 Route::get('listSuppliers', [SuppliersController::class, 'listSuppliers'])->name('listSuppliers');
 Route::put('updateSuppliers', [SuppliersController::class, 'updateSuppliers'])->name('updateSuppliers');
 Route::delete('deleteSuppliers', [SuppliersController::class, 'deleteSuppliers'])->name('deleteSuppliers');
+
+//Rotas para os Products
+Route::post('addProduct', [ProductsController::class, 'addProduct'])->name('addProduct');
+Route::get('listProduct', [ProductsController::class, 'listProduct'])->name('listProduct');
+Route::put('updateProduct', [ProductsController::class, 'updateProduct'])->name('updateProduct');
+Route::delete('deleteProduct', [ProductsController::class, 'deleteProduct'])->name('deleteProduct');
